d277ff62dd231fa769c5aa27ad2708d625744404
初志鑫 <1204505056@qq.com>
2020-04-09 13:26:02 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ touch .nojekyll
 git init
 git add -A
 git commit -m 'deploy'
+git push -f "https://${access_token}@gitee.com/chu1204505056/chu1204505056.git" master:gh-pages
 git push -f "https://${access_token}@gitee.com/chu1204505056/byui.git" master:gh-pages
 git push -f "https://${access_token}@github.com/chuzhixin/vue-element-admin-beautiful.git" master:gh-pages
 cd -
